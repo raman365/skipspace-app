1da329a8ba015143808fcb5a15cbbc8c5825302e
B-A-W <builtbybo.dev@gmail.com>
2023-09-29 11:22:09 +0100

    added styles to signed in dashboard

--- src/navigation/UnauthorisedStack.tsx
+++ src/navigation/UnauthorisedStack.tsx
@@ -26,15 +26,12 @@ export const UnauthorisedStack = () => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				{/* 3 Register */}
+				{/* 1 Welcome/how to */}
 				<Stack.Screen
-					name='SignUp'
-					component={SignUp}
-					options={{
-						headerShown: false,
-					}}
+					name='WelcomeHowTo'
+					component={WelcomeHowTo}
+					options={{ headerShown: false }}
 				/>
-
 				{/* 2 SignIn */}
 				<Stack.Screen
 					name='AuthDashboard'
@@ -44,11 +41,13 @@ export const UnauthorisedStack = () => {
 					}}
 				/>
 
-				{/* 1 Welcome/how to */}
+				{/* 3 Register */}
 				<Stack.Screen
-					name='WelcomeHowTo'
-					component={WelcomeHowTo}
-					options={{ headerShown: false }}
+					name='SignUp'
+					component={SignUp}
+					options={{
+						headerShown: false,
+					}}
 				/>
 
 				{/*  4 Verify Email */}
--- src/screens/AuthorisedStack/SignedInDashboard.tsx
+++ src/screens/AuthorisedStack/SignedInDashboard.tsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React from 'react';
+import { COLORS } from '../../../constants/theme';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HeaderComponent from '../../components/Header';
+import { Button } from '@rneui/themed';
 
 const SignedInDashboard = () => {
 	return (
-		<View>
-			<Text>SignedInDashboard</Text>
-		</View>
+		<SafeAreaProvider style={{ backgroundColor: COLORS.bgBlue }}>
+			<HeaderComponent />
+			<View style={styles.centerContainer}>
+				{/* <Text>SignedInDashboard</Text> */}
+
+				<View style={{ paddingVertical: 40, paddingHorizontal: 30 }}>
+					<Button
+						title='Search for SkipSpace'
+						buttonStyle={{
+							backgroundColor: COLORS.bgGreen,
+							borderRadius: 5,
+							paddingVertical: 15,
+						}}
+						titleStyle={{
+							fontWeight: '700',
+							fontSize: 16,
+							color: COLORS.bgBlue,
+						}}
+						onPress={() => console.log('aye')}
+					/>
+				</View>
+				<View style={{ paddingVertical: 40, paddingHorizontal: 30 }}>
+					<Button
+						title='View active vouchers'
+						buttonStyle={{
+							backgroundColor: COLORS.bgGreen,
+							borderRadius: 5,
+							paddingVertical: 15,
+						}}
+						titleStyle={{
+							fontWeight: '700',
+							fontSize: 16,
+							color: COLORS.bgBlue,
+						}}
+						onPress={() => console.log('aye')}
+					/>
+				</View>
+			</View>
+		</SafeAreaProvider>
 	);
 };
+
+const styles = StyleSheet.create({
+	centerContainer: {
+		paddingHorizontal: 20,
+		display: 'flex',
+		justifyContent: 'center',
+		// flex: 1,
+	},
+});
 
 export default SignedInDashboard;
