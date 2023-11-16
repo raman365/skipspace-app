2e76ee64661654c7ce8a419c0279a16eca4f3e24
B-A-W <builtbybo.dev@gmail.com>
2023-11-16 12:13:13 +0000

    styles changes to skip options sheet

--- src/components/BottomSheet/SkipOptionSheet.tsx
+++ src/components/BottomSheet/SkipOptionSheet.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { BottomSheet, ListItem } from '@rneui/themed';
+import { BottomSheet, Icon, ListItem } from '@rneui/themed';
 
 import { COLORS, FONTSIZES } from '../../../constants/theme';
 import ScreenTitle from '../ScreenTitle';
@@ -42,68 +42,142 @@ const SkipOptionsSheet: React.FC<ISkipOptionsSheetProps> = ({
 				backgroundColor: COLORS.white,
 				flex: 1,
 				justifyContent: 'space-between',
-				padding: 20,
 			}}
 		>
-			<View style={styles.top}>
-				{/* <ScreenTitle title={`Results from ${councilName}`} /> */}
-				<View style={{ paddingTop: 20 }}>
-					<ScreenTitle title={`Selected SkipSpace `} />
-					<TouchableOpacity onPress={onCancelPress}>
-						<Text>Back</Text>
-					</TouchableOpacity>
-				</View>
-				<Text
-					style={{
-						textAlign: 'center',
-						paddingVertical: 10,
-						fontSize: FONTSIZES.xl,
-					}}
-				>
-					{councilName} council
-				</Text>
-			</View>
-			<View style={styles.middle}>
-				<Text>SkipSpace Details:</Text>
+			<View style={{ marginVertical: 20 }}>
+				<View style={styles.top}>
+					{/* <ScreenTitle title={`Results from ${councilName}`} /> */}
 
-				<View style={{ flexDirection: 'row', marginVertical: 10 }}>
-					<Text style={{ fontWeight: 'bold' }}>Name of Skip company: </Text>
-					{/* <Text>{skipCompany}</Text> */}
-					<Text>{skipCompany}</Text>
+					<View
+						style={{
+							paddingVertical: 20,
+							flexDirection: 'row',
+							alignItems: 'center',
+							justifyContent: 'center',
+							// justifyContent: 'center',
+							alignContent: 'space-between',
+						}}
+					>
+						<TouchableOpacity onPress={onCancelPress} style={{ padding: 5 }}>
+							<Icon
+								name='arrow-left'
+								type='feather'
+								color={COLORS.bgBlue}
+								size={40}
+							/>
+						</TouchableOpacity>
+						{/* <View style={{ paddingHorizontal: 5, width: 50 }}></View> */}
+						<Text
+							style={{
+								textAlign: 'center',
+								fontFamily: 'Tungsten_SemiBold',
+								color: COLORS.bgBlue,
+								fontSize: FONTSIZES['5xl'],
+							}}
+						>
+							Selected SkipSpace
+						</Text>
+					</View>
+
+					{/* <Text
+						style={{
+							textAlign: 'center',
+							paddingVertical: 10,
+							fontSize: FONTSIZES.xl,
+						}}
+					>
+						{councilName} council
+					</Text> */}
+
+					<View style={{ borderColor: COLORS.bgGreen, borderWidth: 2 }} />
+				</View>
+				<View style={{ padding: 20, marginBottom: 50 }}>
+					<View style={styles.middle}>
+						<View style={{ marginVertical: 10 }}>
+							<Text
+								style={{
+									fontWeight: 'bold',
+									fontSize: FONTSIZES.xl,
+									paddingVertical: 5,
+								}}
+							>
+								Borough:{' '}
+							</Text>
+
+							<Text style={{ fontSize: FONTSIZES.xl }}>{councilName}</Text>
+						</View>
+						<View style={{ marginVertical: 10 }}>
+							<Text
+								style={{
+									fontWeight: 'bold',
+									fontSize: FONTSIZES.xl,
+									paddingVertical: 5,
+								}}
+							>
+								Name of Skip company:{' '}
+							</Text>
+							{/* <Text>{skipCompany}</Text> */}
+							<Text style={{ fontSize: FONTSIZES.xl }}>{skipCompany}</Text>
+						</View>
+
+						<View style={{ marginVertical: 10 }}>
+							<Text
+								style={{
+									fontWeight: 'bold',
+									fontSize: FONTSIZES.xl,
+									paddingVertical: 5,
+								}}
+							>
+								Address:{' '}
+							</Text>
+							<Text style={{ fontSize: FONTSIZES.xl }}>
+								{skipCompanyAddress}
+							</Text>
+						</View>
+
+						<View style={{ paddingVertical: 10 }}>
+							{/* convert google map link to component */}
+							<Text
+								style={{
+									textDecorationLine: 'underline',
+									textAlign: 'center',
+									fontSize: FONTSIZES.xl,
+									fontWeight: 'bold',
+									color: COLORS.bgBlue,
+								}}
+							>
+								View on Maps
+							</Text>
+						</View>
+					</View>
+					<View style={styles.bottom}>
+						<View
+							style={{
+								borderColor: COLORS.bgBlue,
+								borderWidth: 2,
+								padding: 20,
+							}}
+						>
+							<Text style={{ textAlign: 'center', fontSize: FONTSIZES.large }}>
+								After confirming you'll receive a one-time voucher to use at
+								your selected SkipSpace site.
+							</Text>
+						</View>
+
+						<View style={{ paddingTop: 20 }}>
+							<StandardButton
+								buttonLabel={'Confirm Voucher'}
+								onPress={onVoucherPress}
+								bgGreen
+								fontBlue={false}
+							/>
+						</View>
+					</View>
 				</View>
 
-				<View style={{ marginVertical: 10 }}>
-					<Text style={{ fontWeight: 'bold' }}>Address: </Text>
-					<Text>{skipCompanyAddress}</Text>
-				</View>
+				{/* <View style={{ paddingTop: 10, paddingBottom: 10 }}> */}
 
-				<View>
-					{/* convert google map link to component */}
-					<Text style={{ textDecorationLine: 'underline' }}>View on Maps</Text>
-				</View>
-			</View>
-			<View style={styles.bottom}>
-				<View
-					style={{ borderColor: COLORS.bgBlue, borderWidth: 2, padding: 20 }}
-				>
-					<Text style={{ textAlign: 'center', fontSize: FONTSIZES.large }}>
-						After confirming you'll receive a one-time voucher to use at your
-						selected SkipSpace site.
-					</Text>
-				</View>
-
-				<View style={{ paddingTop: 20 }}>
-					<StandardButton
-						buttonLabel={'Confirm Voucher'}
-						onPress={onVoucherPress}
-						bgGreen
-						fontBlue={false}
-					/>
-				</View>
-			</View>
-			{/* <View style={{ paddingTop: 10, paddingBottom: 10 }}> */}
-
-			{/* <View style={styles.mainContainer}>
+				{/* <View style={styles.mainContainer}>
 				<View style={{ paddingTop: 10, paddingBottom: 10 }}>
 					<ScreenTitle title={`Results from ${councilName}`} />
 				</View>
@@ -117,6 +191,7 @@ const SkipOptionsSheet: React.FC<ISkipOptionsSheetProps> = ({
 					/>
 				</View>
 			</View> */}
+			</View>
 		</BottomSheet>
 	);
 };
@@ -127,15 +202,9 @@ const styles = StyleSheet.create({
 	mainContainer: {
 		backgroundColor: COLORS.white,
 		padding: 30,
-
 		flex: 1,
 		justifyContent: 'space-between',
-
 		margin: 10,
-		// height: '100%',
-		// borderTopLeftRadius: 30,
-		// borderTopRightRadius: 30,
-		// flex: 1,
 	},
 	container: {
 		flex: 1,
@@ -145,13 +214,13 @@ const styles = StyleSheet.create({
 		margin: 10,
 	},
 	top: {
-		flex: 0.3,
-		height: windowHeight / 3,
-		paddingTop: 20,
+		// flex: 0.3,
+		height: windowHeight / 5,
+		paddingVertical: 30,
 	},
 
 	middle: {
-		flex: 0.3,
+		// flex: 0.3,
 		height: windowHeight / 3,
 		margin: 10,
 	},
