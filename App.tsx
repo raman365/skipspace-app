512997e749b10131ba6813ea6bed9d4b82d64088
B-A-W <builtbybo.dev@gmail.com>
2024-02-06 23:07:13 +0000

    removed commented out code

--- App.tsx
+++ App.tsx
@@ -1,27 +1,3 @@
-// import { ThemeProvider } from '@rneui/themed';
-// import 'expo-splash-screen';
-
-// import { theme } from './constants/theme';
-// // import RootNavigation from './src/navigation';
-// import CustomStatusBar from './src/components/StatusBar';
-// import AuthorisedStack from './src/navigation/AuthorisedStack';
-// import { UnauthorisedStack } from './src/navigation/UnauthorisedStack';
-// import { NavigationContainer } from '@react-navigation/native';
-// // import useAuth from './hooks/useAuth';
-// import RootNavigation from './src/navigation';
-// import Providers from './src/navigation';
-// AppRegistry.registerComponent(appName, () => App);`
-
-// // import { AuthProvider } from './src/context/AuthProvider';
-
-// export default function App() {
-// 	return (
-// 		<ThemeProvider theme={theme}>
-// 			<CustomStatusBar />
-// 			<Providers />
-// 		</ThemeProvider>
-// 	);
-// }
 import 'react-native-gesture-handler';
 
 import AuthorisedStack from './src/navigation/AuthorisedStack';
@@ -32,8 +8,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import * as SplashScreen from 'expo-splash-screen';
 import useAuth from './src/hooks/useAuth';
 import { useState, useCallback } from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
-import { COLORS } from './constants/theme';
+import { StyleSheet } from 'react-native';
 export default function App() {
 	const { user, isAuthenticated: authLoading } = useAuth();
 	const [loading, setLoading] = useState<boolean>(true);
@@ -47,16 +22,6 @@ export default function App() {
 	const onLayoutRootView = useCallback(async () => {
 		if (fontsLoaded || fontError) {
 			await SplashScreen.hideAsync();
-			// setTimeout(() => {
-			// setLoading(false);
-			// }, 1000);
-			// try {
-			// 	// setLoading(false);
-
-			// 	await SplashScreen.hideAsync();
-			// } catch (error) {
-			// 	console.error('Error hiding splash screen:', error);
-			// }
 		}
 	}, [fontsLoaded, fontError]);
 
@@ -68,13 +33,6 @@ export default function App() {
 		<NavigationContainer onReady={onLayoutRootView} independent={true}>
 			{user ? <AuthorisedStack /> : <UnauthorisedStack />}
 		</NavigationContainer>
-		// // <View style={styles.container}>
-		// 	{/* {authLoading || user ? (
-		// 		<ActivityIndicator size={'large'} color={COLORS.bgGreen} />
-		// 	) : ( */}
-
-		// 	{/* )} */}
-		// // </View>
 	);
 }
 
